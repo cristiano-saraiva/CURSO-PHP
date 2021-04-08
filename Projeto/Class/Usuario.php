8d3fb0c0df0445962198fd45977d14d4e2e72a30
cristiano-saraiva <cristiano.saraivamello@outlook.com>
2021-04-08 16:24:30 -0300

    Criação da classe usuario

--- Projeto/Class/Usuario.php
+++ Projeto/Class/Usuario.php
@@ -3,5 +3,114 @@
 
 class Usuario
 {
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+//    /**
+//     * Usuario constructor.
+//     * @param $idusuario
+//     * @param $deslogin
+//     * @param $dessenha
+//     * @param $dtcadastro
+//     */
+//    public function __construct($idusuario, $deslogin, $dessenha, $dtcadastro)
+//    {
+//        $this->idusuario = $idusuario;
+//        $this->deslogin = $deslogin;
+//        $this->dessenha = $dessenha;
+//        $this->dtcadastro = $dtcadastro;
+//    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getIdusuario()
+    {
+        return $this->idusuario;
+    }
+
+    /**
+     * @param mixed $idusuario
+     */
+    public function setIdusuario($idusuario)
+    {
+        $this->idusuario = $idusuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeslogin()
+    {
+        return $this->deslogin;
+    }
+
+    /**
+     * @param mixed $deslogin
+     */
+    public function setDeslogin($deslogin)
+    {
+        $this->deslogin = $deslogin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDessenha()
+    {
+        return $this->dessenha;
+    }
+
+    /**
+     * @param mixed $dessenha
+     */
+    public function setDessenha($dessenha)
+    {
+        $this->dessenha = $dessenha;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtcadastro()
+    {
+        return $this->dtcadastro;
+    }
+
+    /**
+     * @param mixed $dtcadastro
+     */
+    public function setDtcadastro($dtcadastro)
+    {
+        $this->dtcadastro = $dtcadastro;
+    }
+
+    public function loadById($id)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID" => $id
+        ));
+        if (count($result) > 0) {
+            $row = $result[0];
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+
+    public function __toString()
+    {
+        return json_encode(array(
+            "id"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
 
 }
