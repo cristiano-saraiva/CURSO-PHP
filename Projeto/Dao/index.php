fbd32a8f35f0f3748ac6436c15a8c77475e587e3
cristiano-saraiva <cristiano.saraivamello@outlook.com>
2021-04-09 00:55:35 -0300

    Manipular Arquivos

--- Projeto/Dao/index.php
+++ Projeto/Dao/index.php
@@ -15,23 +15,16 @@ require_once "config.php";
 //echo $usuario;
 
 //////////////// Carrega uma lista de usuario /////////////////////////////
-
 /*$lista = Usuario::getList();
-
 echo json_encode($lista);*/
 
 //////////////// Procurar em uma lista de usuario /////////////////////////////
-
 /*$busca = Usuario::search("Jo");
-
 echo json_encode($busca);*/
 
 //////////////// Procurar usuario por login e senha  /////////////////////////////
-
 /*$usuario = new Usuario();  /*criar classe Usuario mais uma vez*/
-
 /*$usuario->login("João", "%$#@!");
-
 echo $usuario;*/
 
 //////////////// insert usuario   /////////////////////////////
@@ -43,13 +36,10 @@ echo $aluno;*/
 /*$usuario = new Usuario();
 $usuario->loadById(8);
 $usuario->update("professor", 'qwerty');
-
 echo $usuario;*/
 
 //////////////// Deletando usuario   /////////////////////////////
-
 $usuario = new Usuario();
 $usuario->loadById(8);
 $usuario->delete();
-
 echo $usuario;
