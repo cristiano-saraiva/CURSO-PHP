9334c85b38eda2cb3c3bbb5c4a8fcd8bf7250cb1
cristiano-saraiva <cristiano.saraivamello@outlook.com>
2021-04-15 01:14:17 -0300

    files

---
+++ Projeto/FOPEN/Class/Usuario.php
@@ -0,0 +1,186 @@
+<?php
+
+
+class Usuario
+{
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    /**
+     * Usuario constructor.
+     * @param $deslogin
+     * @param $dessenha
+     */
+    public function __construct($deslogin = "", $dessenha = "")
+    {
+        $this->setDeslogin($deslogin);
+        $this->setDessenha($dessenha);
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getIdusuario()
+    {
+        return $this->idusuario;
+    }
+
+    /**
+     * @param mixed $idusuario
+     */
+    public function setIdusuario($idusuario)
+    {
+        $this->idusuario = $idusuario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDeslogin()
+    {
+        return $this->deslogin;
+    }
+
+    /**
+     * @param mixed $deslogin
+     */
+    public function setDeslogin($deslogin)
+    {
+        $this->deslogin = $deslogin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDessenha()
+    {
+        return $this->dessenha;
+    }
+
+    /**
+     * @param mixed $dessenha
+     */
+    public function setDessenha($dessenha)
+    {
+        $this->dessenha = $dessenha;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDtcadastro()
+    {
+        return $this->dtcadastro;
+    }
+
+    /**
+     * @param mixed $dtcadastro
+     */
+    public function setDtcadastro($dtcadastro)
+    {
+        $this->dtcadastro = $dtcadastro;
+    }
+
+    public function loadById($id)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID" => $id
+        ));
+        if (count($result) > 0) {
+            $this->setData($result[0]);
+        }
+    }
+
+    public static function getList()
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+    }
+
+    public static function search($login)
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ":SEARCH" => "%" . $login . "%"
+        ));
+
+    }
+
+    public function login($login, $password)
+    {
+        $sql = new Sql();
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+            ":LOGIN" => $login,
+            ":PASSWORD" => $password
+        ));
+        if (count($result) > 0) {
+            $this->setData($result[0]);
+        } else {
+            throw new Exception("Login e ou senha invalidos");
+        }
+    }
+
+    public function setData($data)
+    {
+        $this->setIdusuario($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    }
+
+    public function insert()
+    {
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ":LOGIN" => $this->getDeslogin(),
+            ":PASSWORD" => $this->getDessenha()
+        ));
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        } else {
+            throw new Exception("Login e ou senha invalidos");
+        }
+    }
+
+    public function update($login, $password)
+    {
+        $this->setDeslogin($login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+        $results = $sql->select("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+            ":LOGIN" => $this->getDeslogin(),
+            ":PASSWORD" => $this->getDessenha(),
+            ":ID" => $this->getIdusuario()
+        ));
+    }
+
+    public function delete()
+    {
+        $sql = new Sql();
+        $results = $sql->select("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID" => $this->getIdusuario()
+        ));
+
+        $this->setIdusuario(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
+    public function __toString()
+    {
+        return json_encode(array(
+            "id" => $this->getIdusuario(),
+            "deslogin" => $this->getDeslogin(),
+            "dessenha" => $this->getDessenha(),
+            "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
+
+}
---
+++ Projeto/FOPEN/Dao/index.php
@@ -0,0 +1,45 @@
+<?php
+require_once "config.php";
+
+//$sql = new Sql();
+
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//
+//echo json_encode($usuarios);
+
+  /*traz um usuario apenas*/
+//$usuario = new Usuario();  /*criar classe Usuario mais uma vez*/
+//
+//$usuario->loadById(3);
+//
+//echo $usuario;
+
+//////////////// Carrega uma lista de usuario /////////////////////////////
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//////////////// Procurar em uma lista de usuario /////////////////////////////
+/*$busca = Usuario::search("Jo");
+echo json_encode($busca);*/
+
+//////////////// Procurar usuario por login e senha  /////////////////////////////
+/*$usuario = new Usuario();  /*criar classe Usuario mais uma vez*/
+/*$usuario->login("João", "%$#@!");
+echo $usuario;*/
+
+//////////////// insert usuario   /////////////////////////////
+/*$aluno = new Usuario("Francisco", "@fran");
+$aluno->insert();
+echo $aluno;*/
+
+//////////////// Atualizando usuario   /////////////////////////////
+/*$usuario = new Usuario();
+$usuario->loadById(8);
+$usuario->update("professor", 'qwerty');
+echo $usuario;*/
+
+//////////////// Deletando usuario   /////////////////////////////
+$usuario = new Usuario();
+$usuario->loadById(8);
+$usuario->delete();
+echo $usuario;
